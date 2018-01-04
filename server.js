d61c5993e764ae88a676422377f9f30c3d9588c5
unknown <noumannawaz19@yahoo.com>
2018-01-04 20:28:00 +0500

    projects page added

--- server.js
+++ server.js
@@ -64,6 +64,15 @@ app.get('/about', (req, res) => {
     });
 });
 
+
+app.get('/projects', (req, res) => {
+    res.render('projects', {
+        pageTitle: 'Projects page'
+
+    });
+});
+
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'sory boi, take your 404'
